01fccad3fcce86bf792c45a57280f0543df36281
~Mads~ <madelinerodriguez.04@gmail.com>
2025-07-16 16:06:29 -0400

    hmm
    rev 2

--- cursor.js
+++ cursor.js
@@ -1,1 +1,18 @@
 // JavaScript for sparkle cursor goes here
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', (e) => {
+  cursor.style.top = `${e.pageY}px`;
+  cursor.style.left = `${e.pageX}px`;
+});
+
+const sparkleTrail = () => {
+  const sparkle = document.createElement('div');
+  sparkle.className = 'sparkle';
+  sparkle.style.left = cursor.style.left;
+  sparkle.style.top = cursor.style.top;
+  document.body.appendChild(sparkle);
+  setTimeout(() => sparkle.remove(), 500);
+};
+
+setInterval(sparkleTrail, 60);
